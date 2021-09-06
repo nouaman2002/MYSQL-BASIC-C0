e697da1ae09ab35c36ab60768d379656165bd98d
Nouaman <nouaman.abdelghani@student.rocva.nl>
2021-09-06 09:55:43 +0200

    hfh

--- 02-Select/taak02-SELECT-WHERE/antwoorden.sql
+++ 02-Select/taak02-SELECT-WHERE/antwoorden.sql
@@ -1,19 +1,19 @@
--- Opdracht 1 
+-- Opdracht 1  SELECT * FROM `jaar2015` WHERE rank=7
 
--- Opdracht 2 
+-- Opdracht 2 SELECT * FROM `jaar2016` WHERE rank
 
--- Opdracht 3
+-- Opdracht 3 SELECT * FROM `jaar2015` WHERE country='Netherlands'
 
--- Opdracht 4
+-- Opdracht 4 SELECT * FROM `jaar2016` WHERE country='Netherlands'
 
--- Opdracht 5
+-- Opdracht 5 SELECT * FROM `jaar2015` WHERE region='North America'
 
--- Opdracht 6 
+-- Opdracht 6 SELECT country,score FROM `jaar2016` WHERE country='Panama' AND rank=25
 
--- Opdracht 7 
+-- Opdracht 7 SELECT country,score FROM `jaar2016` WHERE score>7087
 
--- Opdracht 8
+-- Opdracht 8 SELECT country,score FROM `jaar2016` WHERE rank<5
 
--- Opdracht 9
+-- Opdracht 9 SELECT country,score FROM `jaar2015` WHERE country='Ireland'
 
--- Opdracht 10
+-- Opdracht 10 SELECT country,region FROM `jaar2015` WHERE region='Latin America and Caribbean'
